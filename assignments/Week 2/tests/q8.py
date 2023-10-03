b83f9c4521240b10a0fb118bd6498390ecd3f8cf
Ananay Sharma <114554856+ananaysharma@users.noreply.github.com>
2023-10-03 15:39:16 -0700

    Delete assignments/Week 2/tests directory

--- assignments/Week 2/tests/q8.py
+++
@@ -1,9 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q8',
-    'points': 2,
-    'suites': [   {   'cases': [{'code': '>>> assert np.array_equal(dot_product_array, dot_product_list)\n', 'hidden': True, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
